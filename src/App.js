87d969b3814b8a24b1b0ae8796f69507b84e8d38
Arunkumar Nattamai Hariharan <anattama@redhat.com>
2023-03-21 10:01:22 -0400

    Updated backend service URL

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
-    fetch('https://nodebackendservice-dev-env.apps.cluster-xr44w.xr44w.sandbox444.opentlc.com/products').then((response) => response.json())
+    fetch('https://backendnodeservice-dev-env.apps.cluster-66pdf.66pdf.sandbox2818.opentlc.com/products').then((response) => response.json())
       .then((data) => {
         console.log(data);
         setIsLoaded(true);
